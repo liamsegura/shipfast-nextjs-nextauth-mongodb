60e610eb466fe8c2af61c5312386a73c57159d8b
Liam Segura <liamdiegosegura@gmail.com>
2024-10-01 20:45:37 +0100

    image

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,6 +2,7 @@
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default async function Navbar() {
   const session = await getServerSession(authOptions)
@@ -10,6 +11,12 @@ export default async function Navbar() {
       {session ? (
         <div>
           <p>Signed in as {session.user.email}</p>
+          <Image
+            src={session.user.image}
+            alt="User image"
+            width={50}
+            height={50}
+          />
           <div className="flex gap-2">
             <Link href="/">Home</Link>
             <Link href="/admin">Admin</Link>
